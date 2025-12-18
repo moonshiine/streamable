860fe09acd65244dd7f9a26a6557e3039d856e6e
moon <martinfornitesigma@gmail.com>
2025-12-18 00:46:58 -0300

    Create args.lua

---
+++ args.lua
@@ -0,0 +1,8 @@
+{
+  "2788229376": "UpdateMousePosI2",
+  "7213786345": "UpdateMousePosI2",
+  "16033173781": "UpdateMousePosI2",
+  "9825515356": "MousePosUpdate",
+  "131984654356230": "UpdateMousePos",
+  "108590068984015": "DEAHOODMOUSEPOSx3^3"
+}
